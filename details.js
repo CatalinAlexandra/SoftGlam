a799c83290a9c671d08eae41ef3e21a5e86c168c
Radu Catalina <raducatalinalexandra@gmail.com>
2025-11-01 18:13:28 +0200

    feat: detail page

---
+++ details.js
@@ -0,0 +1,11 @@
+const searchParams = new URLSearchParams(window.location.search);
+const id = searchParams.get('id');
+
+const URL = 'https://68e6748f21dd31f22cc5bac6.mockapi.io/service';
+
+fetch(`${URL}/${id}`)
+  .then((response) => response.json())
+  .then(
+    (product) =>
+      (document.querySelector('.details-container').innerHTML = product.details)
+  );
